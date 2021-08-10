bc9ee6e0ad3fc78efdbb98da03cb29778686653a
Praveen Parashar <parashar.praveen@hotmail.com>
2021-08-10 20:45:24 +0530

    avatar image added

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210810.2038';
+const cacheName = 'chirpy-20210810.2045';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
