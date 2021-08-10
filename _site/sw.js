84b9f2a6c62e3c4cc810f609d840427bc652eeec
Praveen Parashar <parashar.praveen@hotmail.com>
2021-08-10 16:30:56 +0530

    build

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210810.1624';
+const cacheName = 'chirpy-20210810.1630';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
