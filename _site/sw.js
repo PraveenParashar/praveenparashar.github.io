fdd49207458cf8049c9ef1547258b692af82f1ea
“praveenparashar” <Praveen.Parashar.2007@gmail.com>
2021-12-08 13:55:10 +0530

    Compiled into mac

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210911.0955';
+const cacheName = 'chirpy-20211208.1354';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
