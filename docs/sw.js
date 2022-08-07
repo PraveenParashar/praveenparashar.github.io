a265c12c3242f60d63f1f076d885ec70a2a081ff
“praveenparashar” <Praveen.Parashar.2007@gmail.com>
2022-08-07 23:29:19 +0530

    updated post

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20211213.2243';
+const cacheName = 'chirpy-20220807.2324';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
